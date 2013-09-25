9761084830d73db21291875576bbd1b257ff30f6
uenowataru <ueno.wataru@live.com>
2013-09-25 12:56:01 -0400

    separated files.

---
+++ Node.py
@@ -0,0 +1,71 @@
+class Node:
+	def __init__(self, orderlist, menuitems, parent):
+        self.orderlist = orderlist
+        self.menuitems = menuitems
+        self.parent = parent
+        self.successors = []
+        self.frequency = 0
+        self.product = 1
+        for p in menuitems[:,0]:
+        	self.product*=p
+        __createSuccessors()
+
+    #returns -1 if node not subset
+    #returns 0 if node is subset of this
+    #returns 1 if node is equal to this
+    def isSubset(self, node):
+    	if(node.getProduct()==self.product):
+    		return 1
+    	if(self.product%node.getProduct()==0):
+    		return 0
+    	return -1
+
+    #slower version
+    def isSubset2(self, node):
+    	for item in node.getOrders():
+    		found = False
+    		for item2 in self.orderlist:
+    			if item==item2:
+    				found = True
+    		if not found:
+    			return -1
+    	if(len(node.getOrders())==len(self.orderlist)):
+    		return 1
+    	return 0
+
+    def getParent(self):
+    	return self.parent
+
+    def getSuccessors(self):
+        return self.successors
+
+    def addSuccessor(self, node):
+        self.successors.append(node)
+
+    def __createSuccessors(self):
+    	for index in range(menuitems):
+  		  	temporder = list(orderlist)
+  		  	adding = True
+  		  	for item in tempmenu:
+  		  		finding = True
+  		  		for item2 in temporder:
+  		  			if(finding and item==item2)
+  		  				finding = False
+  		  		if(adding and finding):
+  		  			temporder.append(item)
+  		  			adding = False
+  		  	if(not adding):
+	  		  	successors.append(Node(temporder,menuitems,self)
+
+    def getCost(self):
+        return self.cost
+
+    def getFrequency():
+    	return self.frequency
+
+    def incrementFrequency():
+    	self.frequency += 1
+
+    def getProduct():
+    	return self.product
+
