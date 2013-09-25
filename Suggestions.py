0b8c0fc2de17c4b4014f5ea118e74ae6cf05d83c
uenowataru <ueno.wataru@live.com>
2013-09-24 23:21:31 -0400

    added methods and classes.

--- Suggestions.py
+++ Suggestions.py
@@ -1,5 +1,14 @@
-def getTopSuggestions(Set order):
-	node = rootnode({})
+def run():
+	getTopSuggestions(setup())
+
+def setup():
+	menuitems = {'Fries', 'Rice', 'Burgers', 'Cheeseburgers', 'Coca-cola', 'Fanta'}
+	pastorders = generateOrders(menuitems, 10, 3)
+	root = Node({})
+	
+	return root
+
+def getTopSuggestions(orderlist, root):
 	while(node not order):
 		for x in node.getSuccessors():
 			if x is in order:
@@ -11,3 +20,39 @@ def getTopSuggestions(Set order):
 			maxorder = x.getFrequency()
 			order = x
 	return order
+
+#probability: 1/probability chance of individual menu being ordered
+def generateOrders(menuitems, numorders, probability):
+	pastorders = {}
+	for orderindex in range(numorders):
+		temporder = {}
+		for menuindex in range(menuitems):
+			if random.randint(0,probability-1)<1:
+				temporder.append(menuitems[menuindex])
+		pastorders.append(temporder)
+	return pastorders
+
+class Node:
+	def __init__(self, orderlist, menuitems, parent):
+        self.orderlist = orderlist
+        self.menuitems = menuitems
+        self.parent = parent
+        self.frequency = 1
+
+    def getParent(self):
+    	return self.parent
+
+    def getSuccessors(self):
+        return self.parent
+
+    def setSuccessor(self, node):
+        return self.state
+
+    def getCost(self):
+        return self.cost
+
+    def getFrequency():
+    	return self.frequency
+
+    def incrementFrequency():
+    	self.frequency += 1
